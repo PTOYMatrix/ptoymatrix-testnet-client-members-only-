4c90c8cae828b1d0ad7a2d1547276ae18acec821
Vijay D <vijay@patientory.com>
2018-05-29 16:04:23 -0400

    Initial commit

---
+++ example.js
@@ -0,0 +1,11 @@
+const sdk = require ('./index');
+
+sdk.mobileRegistration.run({mobileCountry: '+1', mobile: '1234567890'}).then(function(data){
+	console.log(data);
+}).catch(function(error){
+	console.log(error);
+});
+
+// sdk. mobileVerification.run({referenceId: 'a631374c-c69d-47e0-a00e-2738b2yp3513', mobileOtp: '318326'}).then(function (data){
+// 	console.log(data);
+// }).catch(console.log);
---
+++ index.js
@@ -0,0 +1,47 @@
+const mobileRegistration = require('./lib/mobileRegistration')
+const mobileVerification = require('./lib/mobileVerification')
+const generateMnemonic = require('./lib/generateMnemonic')
+const ecdsaKeyPairGenerator = require('./lib/ecdsaKeyPairGenerator')
+const dashKeyPairGenerator = require('./lib/dashKeyPairGenerator')
+const rsaKeyPairGenerator = require('./lib/rsaKeyPairGenerator')
+const createSignatureECDSA = require('./lib/createSignatureECDSA')
+const createSignatureRSA = require('./lib/createSignatureRSA')
+const encryptPrivateKey = require('./lib/encryptPrivateKey')
+const decryptPrivateKey = require('./lib/decryptPrivateKey')
+const signUp = require('./lib/signUp')
+const getUserProfile = require('./lib/getUserProfile')
+const login = require('./lib/login')
+const uploadFile = require('./lib/uploadFile')
+const signTransaction = require('./lib/signTransaction')
+const submitTx = require('./lib/submitTx')
+const getAllFiles = require('./lib/getAllFiles')
+const getFile = require('./lib/getFile')
+const dataDecryption = require('./lib/dataDecryption')
+const getBalance = require('./lib/getBalance')
+const transferPTOY = require('./lib/transferPTOY')
+const submitPTOYTx = require('./lib/submitPTOYTx')
+
+module.exports = {
+  mobileRegistration,
+  mobileVerification,
+  generateMnemonic,
+  ecdsaKeyPairGenerator,
+  dashKeyPairGenerator,
+  rsaKeyPairGenerator,
+  createSignatureECDSA,
+  createSignatureRSA,
+  encryptPrivateKey,
+  decryptPrivateKey,
+  signUp,
+  getUserProfile,
+  login,
+  uploadFile,
+  signTransaction,
+  submitTx,
+  getAllFiles,
+  getFile,
+  dataDecryption,
+  getBalance,
+  transferPTOY,
+  submitPTOYTx
+}
